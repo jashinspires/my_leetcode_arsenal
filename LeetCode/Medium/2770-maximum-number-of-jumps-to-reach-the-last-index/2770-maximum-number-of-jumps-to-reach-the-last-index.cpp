9566eb1dbf8c85f663432cd8c0076b69ffd93ab4
Jaswanth <147475797+jashinspires@users.noreply.github.com>
2026-05-10 23:00:08 +0530

    21 ms (49.38%) 68.4 MB (84.78%) 05-10-2026

---
+++ LeetCode/Medium/2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int maximumJumps(vector<int>& nums, int target) {
+        vector<int> dp(nums.size(), -1); dp[0] = 0;
+        for (int j = 1; j < nums.size(); ++j) for (int i = 0; i < j; ++i) if (dp[i] != -1 && abs(1LL * nums[j] - nums[i]) <= target) dp[j] = max(dp[j], dp[i] + 1);
+        return dp.back();
+    }
+};
