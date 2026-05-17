e69714748ac62df44e10a3dc034de834b873c249
Jaswanth <147475797+jashinspires@users.noreply.github.com>
2026-05-17 20:10:57 +0530

    1 ms (54.71%) 38.7 MB (45.74%) 05-17-2026

---
+++ LeetCode/Medium/1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        if (start < 0 || start >= arr.size() || arr[start] < 0) {
+            return false;
+        }
+        if (arr[start] == 0) {
+            return true;
+        }
+        int jump = arr[start];
+        arr[start] = -arr[start];
+        return canReach(arr, start + jump) || canReach(arr, start - jump);
+    }
+};
