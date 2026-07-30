4b4fe7b54d280ef8bc383cb9a15d6cce603f7b90
Jaswanth <147475797+jashinspires@users.noreply.github.com>
2026-07-30 21:33:34 +0530

    3 ms (60.58%) 21 MB (68.89%) 07-30-2026

--- LeetCode/Easy/0136-single-number/0136-single-number.py
+++ LeetCode/Easy/0136-single-number/0136-single-number.py
@@ -1,8 +1,5 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ans = 0
-        for i in nums:
-            ans = ans^i
-        return ans
+        return reduce(operator.xor,nums)
         
         
