d24e1192cea0efe50e899971f1a5d059711edc26
Jaswanth <147475797+jashinspires@users.noreply.github.com>
2026-08-05 11:35:58 +0530

    0 ms (100%) 8.5 MB (64.34%) 08-05-2026

---
+++ LeetCode/Easy/1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+     string s = "";
+     int i = 0 , j = 0 ;
+     int n1 = word1.size();
+     int n2 = word2.size();
+     while( i < n1 || j < n2 ){
+        if(i < n1){
+            s += word1[i++];
+        }
+        if(j < n2){
+            s += word2[j++];
+        }
+     }
+     return s;
+    }
+};
