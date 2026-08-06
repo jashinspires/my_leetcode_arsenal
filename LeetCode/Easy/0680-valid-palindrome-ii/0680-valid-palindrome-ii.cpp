9c091a9be3e6a53aa1274513ca47f6239c06b55d
Jaswanth <147475797+jashinspires@users.noreply.github.com>
2026-08-06 23:11:44 +0530

    3 ms (67.66%) 22.2 MB (56.07%) 08-06-2026

---
+++ LeetCode/Easy/0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool ispalindrome(string& s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int l = 0;
+        int r = s.size() - 1;
+
+        while (l < r) {
+            if (s[l] != s[r]) {
+                return ispalindrome(s, l + 1, r) || ispalindrome(s, l, r - 1);
+            }
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+};
