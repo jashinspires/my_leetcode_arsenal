4e720db10baa5cc76deeca65c181b84d9c792970
Jaswanth <147475797+jashinspires@users.noreply.github.com>
2026-09-04 08:29:09 +0530

    Time: 0 ms (100%), Space: 31 MB (66.39%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -2,21 +2,16 @@ class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<long long> prefix_max(n);
-        vector<long long> suffix_min(n);
-        int curr_max = nums[0];
-        int curr_min = nums[n-1];
-        prefix_max[0] = nums[0];
+        vector<int> suffix_min(n);
         suffix_min[n-1] = nums[n-1];
-        for(int i = 1 , j = n - 2; (i < n) && (j >= 0) ; i++ , j--){
-            prefix_max[i] = max(nums[i],curr_max);
-            curr_max = max(nums[i],curr_max);
-            suffix_min[j] = min(nums[j],curr_min);
-            curr_min = min(nums[j],curr_min);
+        for(int j = n - 2; j >=0 ; j--){
+            suffix_min[j] = min(nums[j],suffix_min[j+1]);
         }
+        int curr_max = nums[0];
         for(int i = 0 ; i < n ; i++){
-            if(prefix_max[i] - suffix_min[i] <= k){
-               return i;
+            curr_max = max(nums[i],curr_max);
+            if((curr_max - suffix_min[i]) <= k){
+                return i;
             }
         }
         return -1;
